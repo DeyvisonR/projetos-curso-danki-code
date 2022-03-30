0fa0b2f2b5050da6587ba5f369c17b5b1306f652
DeyvisonR <deyvisondosreis17@gmail.com>
2022-03-30 04:33:59 -0300

    att

--- projeto_05/js/functions.js
+++ projeto_05/js/functions.js
@@ -8,11 +8,25 @@ $(function(){
 
     // colocando a animação do scrolltop no menu em todas as paginas
 
-    $('nav ul li a').click(function(){
-        var href = $(this).attr('href');
-        var offSetY = $(href).offset().top;
-        $('html, body').animate({'scrolltop': offSetY});
+    var directory = '/curso/projetos-curso-danki-code/projeto_05/home.html';
+
+    $('[goto="#contato"]').click(function(){
+        location.href = directory +'?contato'
+        return false;
     });
+
+    checkUrl();
+
+
+        function checkUrl(){
+            var url = location.href.split('/');
+            var curPage = url[url.length-1].split('?');
+       
+            if(curPage[1] != undefined && curPage[1] == 'contato'){
+              $('html,body').animate({'scrollTop':$('#contato').offset().top});
+            }
+
+        }
 
     $(window).resize(function(){
         if($('body, html').width() + 17 > 760){
